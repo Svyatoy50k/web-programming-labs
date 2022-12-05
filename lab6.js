7e0b819a4f5adaa18787353f9bbdebe87bd3d415
Kalachev Semen <semen.kalachev.02@mail.ru>
2022-12-05 20:44:50 +0700

    Alert

--- lab6.js
+++ lab6.js
@@ -37,3 +37,34 @@ function setEnglish() {
     document.getElementById('ok-button').value = 'ok';
     document.getElementById('reset-button').value = 'reset';
 }
+
+function showAlert() {
+    let userName = document.getElementById('input-name').value;
+    let userSurname = document.getElementById('input-surname').value;
+    let userAge = document.getElementById('input-age').value;
+    alert('Привет,' + userName + ' ' + userSurname + '\n' + 'Ваш возраст:' + userAge);
+    let r = Math.round(Math.random()*255);
+    let g = Math.round(Math.random()*255);
+    let b = Math.round(Math.random()*255);
+    let color = 'rgb(' + r + ',' + g + ',' + b + ')';
+    document.getElementById('user-data').style.backgroundColor = color;
+}
+
+function function1() {
+    alert('Семён Калачёв сделал эту лабораторную работу');
+    let r = Math.round(Math.random()*255);
+    let g = Math.round(Math.random()*255);
+    let b = Math.round(Math.random()*255);
+    let color = 'rgb(' + r + ',' + g + ',' + b + ')';
+    document.getElementById('head').style.backgroundColor = color;
+}
+
+function function2() {
+    alert('Информация о месте обучения этого студента');
+    let r = Math.round(Math.random()*255);
+    let g = Math.round(Math.random()*255);
+    let b = Math.round(Math.random()*255);
+    let color = 'rgb(' + r + ',' + g + ',' + b + ')';
+    document.getElementById('footer').style.backgroundColor = color;
+}
+
